63a0f4afafeb3a86197e9477c45538d89891aec8
Jan Lohage <git@j2L4e.de>
2017-11-02 09:00:26 +0100

    (socket-commons) migrate from feathers

--- packages/socket-commons/index.d.ts
+++ packages/socket-commons/index.d.ts
@@ -1,8 +1,21 @@
+///<reference types="feathers"/>
+
 declare module '@feathersjs/socket-commons' {
   // (JL) does it have a public API?
+  type Connection = any; // todo: spec connection
+
+  interface Channel {
+    join(...connections: Connection[]): this;
+    leave(...connections: Connection[]): this;
+    filter(callback: (connection: Connection) => boolean): Channel;
+    send(data: any): this;
+  }
 }
 
 declare module '@feathersjs/feathers' {
+  import { Channel } from '@feathersjs/socket-commons';
+  import { HookContext } from '@feathersjs/feathers';
+
   interface ServiceAddons<T, DataTypes> {
     publish(callback: (data: T, hook: HookContext<T>) => Channel): this
     publish(event: string, callback: (data: T, hook: HookContext<T>) => Channel): this
