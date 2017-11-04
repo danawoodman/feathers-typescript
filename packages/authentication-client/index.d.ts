a144918a8178282af94588394ab63fb84f04af16
Jan Lohage <git@j2L4e.de>
2017-11-04 17:27:55 +0100

    (authentication-client) first commit

---
+++ packages/authentication-client/index.d.ts
@@ -0,0 +1,76 @@
+declare module '@feathersjs/authentication-client' {
+  function feathersAuthClient(config?: FeathersAuthClientConfig): () => void;
+
+  export default feathersAuthClient;
+
+  export interface FeathersAuthClientConfig {
+    storage?: Storage;
+    header?: string;
+    cookie?: string;
+    storageKey?: string;
+    jwtStrategy?: string;
+    path?: string;
+    entity?: string;
+    service?: string;
+  }
+
+  export interface FeathersAuthCredentials {
+    strategy: string;
+
+    [index: string]: any;
+  }
+
+  export const defaults: {
+    header: string;
+    cookie: string;
+    storageKey: string;
+    jwtStrategy: string;
+    path: string;
+    entity: string;
+    service: string;
+    timeout: number;
+  };
+
+  export interface Passport {
+    setupSocketListeners(): void;
+
+    connected(): Promise<any>;
+
+    authenticate(credentials?: FeathersAuthCredentials): any;
+
+    authenticateSocket(credentials: FeathersAuthCredentials, socket: any, emit: any): any;
+
+    logoutSocket(socket: any, emit: any): Promise<any>;
+
+    logout(): Promise<any>;
+
+    setJWT(data: any): Promise<any>;
+
+    getJWT(): Promise<any>;
+
+    verifyJWT(token: string): Promise<string>;
+
+    payloadIsValid(payload: string): boolean;
+
+    getCookie(name: string): string;
+
+    clearCookie(name: string): null;
+
+    getStorage(storage: any): any;
+  }
+}
+
+declare module '@feathersjs/feathers' {
+  import {
+    FeathersAuthCredentials,
+    Passport
+  } from '@feathersjs/authentication-client';
+
+  interface Application<ServiceTypes> {
+    authenticate(options?: FeathersAuthCredentials): Promise<any>;
+
+    logout(): Promise<void>;
+
+    passport: Passport;
+  }
+}
